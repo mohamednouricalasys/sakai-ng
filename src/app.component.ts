31fbc0e0eb73138179186e841db6b25194a4168f
isgclub <nouri.mohamed.emploi@hotmail.com>
2025-08-07 23:59:42 +0200

    set default strings

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
 import { TranslationService } from './app/core/services/translation.service';
 
 @Component({
@@ -11,9 +12,35 @@ import { TranslationService } from './app/core/services/translation.service';
 })
 export class AppComponent {
     private translationService = inject(TranslationService);
+    private config = inject(PrimeNG);
 
-    ngOnInit() {
+    async ngOnInit() {
         const savedLang = this.translationService.getCurrentLanguage();
         this.translationService.setLanguage(savedLang);
+
+        // Ensure translations are loaded
+        await this.ensureTranslationsLoaded();
+
+        // Set initial PrimeNG translations
+        this.updatePrimeNGTranslations();
+
+        // Listen for language changes
+        effect(() => {
+            const currentLang = this.translationService.currentLanguage();
+            this.updatePrimeNGTranslations();
+        });
+    }
+
+    private async ensureTranslationsLoaded(): Promise<void> {
+        if (!this.translationService.isTranslationsLoaded()) {
+            await this.translationService.init();
+        }
+    }
+
+    private updatePrimeNGTranslations() {
+        if (this.translationService.isTranslationsLoaded()) {
+            const primeNGTranslations = this.translationService.getTranslationObject('primeng');
+            this.config.setTranslation(primeNGTranslations);
+        }
     }
 }
